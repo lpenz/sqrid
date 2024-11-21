b909375a00eb28176b746f3629ddbf80af550c2c
Leandro Lisboa Penz <lpenz@lpenz.org>
2024-11-21 22:49:25 +0000

    intbounded: create with macro to get better inner types
    - MIN and MAX can now have inner type, avoiding usize conversions.
    - We can also now have const and static constructors.
    - This also fixes *IntBounded checked_add and checked_sub, as they
      were not considering min and max.

--- src/sqrid/intbounded.rs
+++ src/sqrid/intbounded.rs
@@ -7,19 +7,9 @@
 //! These are required for integers that are used as coordinates.
 
 use super::error::Error;
-use super::int::Int;
 use super::int::IntExt;
 
-/// A bounded unsigned integer
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub struct UIntBounded<const MIN: usize, const MAX: usize, Inner: Int>(pub Inner);
-
-macro_rules! into_or_panic {
-    ($e:expr) => {{
-        let Ok(value) = $e.try_into() else { panic!() };
-        value
-    }};
-}
+use std::convert::TryFrom;
 
 macro_rules! into_or_oob {
     ($e:expr) => {
@@ -27,58 +17,146 @@ macro_rules! into_or_oob {
     };
 }
 
-impl<const MIN: usize, const MAX: usize, Inner: Int> UIntBounded<MIN, MAX, Inner> {
-    /// Create a new UIntBounded with the given value in it, if it's within bounds
-    pub fn new(v: Inner) -> Result<Self, Error> {
-        if v < into_or_panic!(MIN) || v > into_or_panic!(MAX) {
-            Err(Error::OutOfBounds)
-        } else {
-            Ok(UIntBounded(v))
+macro_rules! intbounded_impl {
+    ($name:ident, $type:ty) => {
+        impl<const MIN: $type, const MAX: $type> $name<MIN, MAX> {
+            /// Create a new bounded int with the given value in it, if it's within bounds
+            pub const fn new(v: $type) -> Result<Self, Error> {
+                if v < MIN || v > MAX {
+                    Err(Error::OutOfBounds)
+                } else {
+                    Ok(Self(v))
+                }
+            }
+
+            /// Create a new bounded int with the given value in it;
+            /// panics if the value is not within bounds
+            pub const fn new_unwrap(v: $type) -> Self {
+                assert!(v >= MIN && v <= MAX);
+                Self(v)
+            }
+
+            /// Create a new bounded int at compile time.
+            ///
+            /// Checks arguments at compile time - for instance, the following
+            /// doesn't compile:
+            /// ```compilation_fail
+            /// const Bounded : sqrid::U8Bounded<0,5> = sqrid::U8Bounded::<0,5>::new_static::<9>();
+            /// ```
+            pub const fn new_static<const V: $type>() -> Self {
+                assert!(V >= MIN && V <= MAX);
+                Self(V)
+            }
+
+            /// Deconstructs an $name and returns the the inner value
+            pub fn into_inner(self) -> $type {
+                self.0
+            }
         }
-    }
 
-    /// Deconstructs an UIntBounded and returns the the inner value
-    pub fn into_inner(self) -> Inner {
-        self.0
-    }
+        impl<const MIN: $type, const MAX: $type> IntExt for $name<MIN, MAX> {
+            #[inline]
+            fn checked_add(self, other: Self) -> Option<Self> {
+                self.0
+                    .checked_add(other.0)
+                    .map(|v| Self(v))
+                    .filter(|v| Self(MIN) <= *v && *v <= Self(MAX))
+            }
+            #[inline]
+            fn checked_sub(self, other: Self) -> Option<Self> {
+                self.0
+                    .checked_sub(other.0)
+                    .map(|v| Self(v))
+                    .filter(|v| Self(MIN) <= *v && *v <= Self(MAX))
+            }
+        }
+
+        impl<const MIN: $type, const MAX: $type> TryFrom<$type> for $name<MIN, MAX> {
+            type Error = super::error::Error;
+            #[inline]
+            fn try_from(value: $type) -> Result<Self, Self::Error> {
+                Self::new(value)
+            }
+        }
+
+        impl<const MIN: $type, const MAX: $type> From<$name<MIN, MAX>> for $type {
+            #[inline]
+            fn from(value: $name<MIN, MAX>) -> $type {
+                value.into_inner()
+            }
+        }
+
+        impl<const MIN: $type, const MAX: $type> From<bool> for $name<MIN, MAX> {
+            #[inline]
+            fn from(value: bool) -> Self {
+                $name(value.into())
+            }
+        }
+
+        impl<const MIN: $type, const MAX: $type> TryFrom<usize> for $name<MIN, MAX> {
+            type Error = super::error::Error;
+            #[inline]
+            fn try_from(value: usize) -> Result<Self, Self::Error> {
+                Ok($name(into_or_oob!(value)?))
+            }
+        }
+
+        impl<const MIN: $type, const MAX: $type> TryFrom<$name<MIN, MAX>> for usize {
+            type Error = super::error::Error;
+            #[inline]
+            fn try_from(value: $name<MIN, MAX>) -> Result<Self, Self::Error> {
+                Ok(into_or_oob!(value)?)
+            }
+        }
+    };
 }
 
-impl<const MIN: usize, const MAX: usize, Inner: Int> IntExt for UIntBounded<MIN, MAX, Inner> {
-    #[inline]
-    fn checked_add(self, other: Self) -> Option<Self> {
-        self.0.checked_add(other.0).map(|v| Self(v))
-    }
-    #[inline]
-    fn checked_sub(self, other: Self) -> Option<Self> {
-        self.0.checked_sub(other.0).map(|v| Self(v))
-    }
-}
+/// A bounded u8
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct U8Bounded<const MIN: u8, const MAX: u8>(pub u8);
+intbounded_impl!(U8Bounded, u8);
 
-impl<const MIN: usize, const MAX: usize, Inner: Int> From<bool> for UIntBounded<MIN, MAX, Inner> {
-    #[inline]
-    fn from(value: bool) -> Self {
-        UIntBounded(value.into())
-    }
-}
+/// A bounded u16
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct U16Bounded<const MIN: u16, const MAX: u16>(pub u16);
+intbounded_impl!(U16Bounded, u16);
 
-impl<const MIN: usize, const MAX: usize, Inner: Int> TryFrom<usize>
-    for UIntBounded<MIN, MAX, Inner>
-{
-    type Error = super::error::Error;
-    #[inline]
-    fn try_from(value: usize) -> Result<Self, Self::Error> {
-        Ok(UIntBounded(into_or_oob!(value)?))
-    }
-}
+/// A bounded u32
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct U32Bounded<const MIN: u32, const MAX: u32>(pub u32);
+intbounded_impl!(U32Bounded, u32);
 
-impl<const MIN: usize, const MAX: usize, Inner: Int> TryFrom<UIntBounded<MIN, MAX, Inner>>
-    for usize
-{
-    type Error = super::error::Error;
-    #[inline]
-    fn try_from(value: UIntBounded<MIN, MAX, Inner>) -> Result<Self, Self::Error> {
-        Ok(into_or_oob!(value)?)
-    }
-}
+/// A bounded u64
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct U64Bounded<const MIN: u64, const MAX: u64>(pub u64);
+intbounded_impl!(U64Bounded, u64);
 
-// UIntBounded impls Int through the blanked implementation
+/// A bounded u128
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct U128Bounded<const MIN: u128, const MAX: u128>(pub u128);
+intbounded_impl!(U128Bounded, u128);
+
+/// A bounded i8
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct I8Bounded<const MIN: i8, const MAX: i8>(pub i8);
+intbounded_impl!(I8Bounded, i8);
+
+/// A bounded i16
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct I16Bounded<const MIN: i16, const MAX: i16>(pub i16);
+intbounded_impl!(I16Bounded, i16);
+
+/// A bounded i32
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct I32Bounded<const MIN: i32, const MAX: i32>(pub i32);
+intbounded_impl!(I32Bounded, i32);
+
+/// A bounded i64
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct I64Bounded<const MIN: i64, const MAX: i64>(pub i64);
+intbounded_impl!(I64Bounded, i64);
+
+/// A bounded i128
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct I128Bounded<const MIN: i128, const MAX: i128>(pub i128);
+intbounded_impl!(I128Bounded, i128);
--- tests/intbounded_tests.rs
+++ tests/intbounded_tests.rs
@@ -2,19 +2,59 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of this source code package.
 
-use sqrid::intbounded::UIntBounded;
-use sqrid::IntExt;
+use sqrid::intbounded::*;
+use sqrid::Int;
 
-type UIB = UIntBounded<0, 20, u8>;
+macro_rules! or_panic {
+    ($e:expr) => {{
+        let Ok(value) = $e else { panic!() };
+        value
+    }};
+}
 
-#[test]
-fn test_basic() {
-    let i5 = UIB::new(5).unwrap();
+fn _test_basic<T: Int>() {
+    let i5 = or_panic!(T::try_from(5));
     assert!(i5 == i5);
     assert!(i5 >= i5);
     assert!(i5 <= i5);
     assert_eq!(i5, i5);
     assert_ne!(i5 < i5, true);
     assert_ne!(i5 > i5, true);
-    assert_eq!(i5.checked_add(i5), UIB::new(10).ok());
+    assert_eq!(i5.checked_add(i5), Some(or_panic!(T::try_from(10))));
+}
+
+#[test]
+fn test_uints() {
+    _test_basic::<u8>();
+    _test_basic::<u16>();
+    _test_basic::<u32>();
+    _test_basic::<u64>();
+    _test_basic::<u128>();
+}
+
+#[test]
+fn test_iints() {
+    _test_basic::<i8>();
+    _test_basic::<i16>();
+    _test_basic::<i32>();
+    _test_basic::<i64>();
+    _test_basic::<i128>();
+}
+
+#[test]
+fn test_uintbounded() {
+    _test_basic::<U8Bounded<0, 20>>();
+    _test_basic::<U16Bounded<0, 20>>();
+    _test_basic::<U32Bounded<0, 20>>();
+    _test_basic::<U64Bounded<0, 20>>();
+    _test_basic::<U128Bounded<0, 20>>();
+}
+
+#[test]
+fn test_iintbounded() {
+    _test_basic::<I8Bounded<0, 20>>();
+    _test_basic::<I16Bounded<0, 20>>();
+    _test_basic::<I32Bounded<0, 20>>();
+    _test_basic::<I64Bounded<0, 20>>();
+    _test_basic::<I128Bounded<0, 20>>();
 }
